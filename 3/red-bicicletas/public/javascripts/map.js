516c6251e55f6bcd21409380f7f0cb0f4f19b6b9
Ignacio Scaduto <nachosca@gmail.com>
2021-06-22 15:32:40 -0300

    added marker to map

--- 3/red-bicicletas/public/javascripts/map.js
+++ 3/red-bicicletas/public/javascripts/map.js
@@ -3,3 +3,5 @@ var map = L.map('main_map').setView([-34.6012, -58.38614], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
+
+L.marker([-34.6012,-58.3861]).addTo(map);
